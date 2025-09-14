891189e9c4a0bd1eea1494bb2074c7cf75758ac1
HabitualCoder <jaydeep.geekcoder@gmail.com>
2025-09-14 21:35:40 +0530

    added UpcomingMoviesSection

---
+++ components/server/UpcomingMoviesSection.tsx
@@ -0,0 +1,14 @@
+import axiosInstance from "@/lib/axiosInstance";
+import { TMDB_API } from "@/lib/constants";
+import MoviesCarousel from "@/components/client/MoviesCarousel";
+
+export default async function UpcomingMoviesSection() {
+  const res = await axiosInstance.get(TMDB_API.UPCOMING_MOVIES);
+  const movies = res.data.results;
+  return (
+    <section className="w-full my-8">
+      <h2 className="text-2xl font-bold mb-4 px-4">Upcoming Movies</h2>
+      <MoviesCarousel movies={movies} />
+    </section>
+  );
+}
